c05915f6f86f131dd82fc86ff4bb9bef2070d16f
aleffalves <aleff.alves44@gmail.com>
2021-08-05 11:12:56 -0300

    "first commit"

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,10 @@
+package academy.devdojo.springboot2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Anime {
+    private String name;
+}
