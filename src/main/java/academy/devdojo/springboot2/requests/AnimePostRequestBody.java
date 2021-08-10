19e1385e27c04249b0dfc76332cd272a17678d87
aleffalves <aleff.alves44@gmail.com>
2021-08-10 11:35:04 -0300

    "criando excecoes"

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,8 +1,13 @@
 package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
